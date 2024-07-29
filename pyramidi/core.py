0a701be3512bb40fc465ac8718b3ae4e3dbb0828
Konrad Swierczek <swierckj@mcmaster.ca>
2024-07-29 14:51:06 -0400

    fix(core): Correct issues with core functions
    - FIx pre-process to avoid voice crossing issues - Fix issues with cut overshooting

--- pyramidi/core.py
+++ pyramidi/core.py
@@ -3,13 +3,17 @@
 ###############################################################################
 # Local Imports
 # Third Party Imports
-from mido import MidiFile, MidiTrack, MetaMessage, Message, tempo2bpm
+from mido import MidiFile, MidiTrack, MetaMessage, Message, tempo2bpm, merge_tracks
 ###############################################################################
 # Constants
-__all__ = ['pre_process']
+__all__ = ['pre_process', "cut"]
 ###############################################################################
-def pre_process(midiFile, savepath: str = None):
-    """Reformat and preprocess a MIDI file for analysis.
+def pre_process(
+    midi_file,
+    savepath: str = None
+):
+    """
+    Reformat and preprocess a MIDI file for analysis.
     Returns a Type 0 Mido MidiFile class object.
 
     Keyword arguments:
@@ -19,44 +23,21 @@ def pre_process(midiFile, savepath: str = None):
     # TODO: Add savepath functionality
     ###########################################################################
     # Read midi file with Mido.
-    midiFile = MidiFile(midiFile)
+    midi_data = MidiFile(midi_file)
     # Create new Type 0 Mido MidiFile class object, add input 'ticks_per_beat'.
-    new = MidiFile(type = 0, ticks_per_beat = midiFile.ticks_per_beat)
-    track = MidiTrack()
-    new.tracks.append(track)
-    # Loop through tracks in midi file.
-    for i in range(len(midiFile.tracks)):
-            # Loop through messages in a given track.
-            for msg in midiFile.tracks[i]:
-                # Filter unneeded messages.
-                # NOTE: 'end_of_track' is filtered due to copies exisiting in
-                #        the case of multiple tracks.
-                if msg.type in ['end_of_track', 'control_change',
-                                'program_change']:
-                    continue
-                # Convert 0 velocity "note_on" to "note_off" messages.
-                # NOTE: According to MIDI standard, "note_on" messages with
-                #       a velocity of zero are equivilant to "note_off".
-                elif msg.type == "note_on" and msg.velocity == 0:
-                    track.append(Message('note_off', 
-                                         note = msg.note,
-                                         velocity = msg.velocity,
-                                         time = msg.time))
-                # Copy all other messages directly.
-                else:
-                    track.append(msg.copy())
-    # Add 'end_of_track' message.
-    track.append(MetaMessage(type = 'end_of_track', time = 1))
+    new_midi = MidiFile(
+        type = 0,
+        ticks_per_beat = midi_data.ticks_per_beat
+    )
+    new_midi.tracks.append(merge_tracks(midi_data.tracks))
     # Return entire Mido MidiFile class object.
-    return new
+    return new_midi
 
 ###############################################################################
-def lenBar(timeSignature: tuple):
+def lenBar(time_signature: tuple):
     """Given a time signature, returns the number of quarter notes per bar."""
-    # Divide 4 by denominator of time signature for standard beat unit.
-    beat_unit = 4 / timeSignature[1]
-    # return numerator of time signature multiplied by standard beat unit.
-    return int(timeSignature[0] * beat_unit)
+    quarter_notes = (time_signature[0]/time_signature[1]) * 4
+    return quarter_notes
 
 ###############################################################################
 def midi_2_key(midiNumber: int):
@@ -77,33 +58,45 @@ def get_timesig(midi_file):
     time_sig = [(msg.numerator, msg.denominator) for msg in midi_file if msg.type == "time_signature"]
     return time_sig[0]
 
-###############################################################################
-def cut(midiFile, measures: float = 8, direct: bool = False):
-    counter = 0
-    ongoing_notes = set()
-    beats_per_bar = lenBar(get_timesig(midiFile))
-    new = MidiFile(type = 0, ticks_per_beat = midiFile.ticks_per_beat)
+# =========================================================================== #
+def cut(midi_data, measures = 8):
+    # variables
+    tpb = midi_data.ticks_per_beat
+    ticks = int()
+    target_ticks = int(
+        [lenBar(
+            (msg.numerator,
+             msg.denominator
+            )
+        ) * measures * tpb for msg in midi_data.tracks[0] if msg.type == "time_signature"
+        ][0])
+    active_notes = dict()
+    # New midi data
+    new = MidiFile(type=0, ticks_per_beat=tpb)
     track = MidiTrack()
     new.tracks.append(track)
-    for i, msg in enumerate(midiFile.tracks[0]):
-    # TOP LEVEL: IS THE COUNTER OVER?
-        if counter < measures:      
-            abs_time = sum(msg.time for msg in track)
-    # SECOND LEVEL: IS THE MESSAGE A TIME SIGNATURE?
+    for msg in midi_data.tracks[0]:
+        if isinstance(msg, MetaMessage):
             if msg.type == "time_signature":
-                    time_sig = (msg.numerator, msg.denominator)
-                    beats_per_bar = lenBar(time_sig)
-            elif msg.type == 'note_on' and msg.velocity > 0:
-                ongoing_notes.add(msg.note)
-            elif msg.type == 'note_off' or msg.type == "note_on" and msg.velocity == 0:
-                ongoing_notes.discard(msg.note)
-            track.append(msg)
-            counter = (msg.time/midiFile.ticks_per_beat)/beats_per_bar + counter
-        elif counter >= measures:
+                time_signature = (msg.numerator, msg.denominator)
+                target_ticks = lenBar(time_signature) * measures * tpb
+        elif isinstance(msg, Message):
+            if msg.type == "note_on" and msg.velocity > 0:
+                active_notes[msg.note] = msg.velocity
+            elif msg.type == "note_off" or (msg.type == "note_on" and msg.velocity == 0):
+                active_notes.pop(msg.note, None)
+        ticks += msg.time
+        # write
+        if ticks > target_ticks:
             break
-    for open_note in ongoing_notes:
-        track.append(Message(type = 'note_off', note = open_note, time = 0))
+        elif ticks == target_ticks and  msg.type == "note_on" and msg.velocity > 0:
+            break
+        else:
+            track.append(msg)
+    # Clean up
+    for leftover in active_notes:
+        track.append(Message(type = "note_off", note = leftover, velocity = active_notes[leftover], time = 0))
     track.append(MetaMessage(type = 'end_of_track', time = 1))
     return new
 
-###############################################################################
+# =========================================================================== #
